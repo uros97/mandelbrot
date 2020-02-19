9346623ba5e91c60d1be76757f4754e24c0e2eda
uros97 <uros.jakovljevic97@gmail.com>
2020-02-19 17:29:58 +0100

    Initial commit - OpenACC version not working

---
+++ mandelbrot.cpp
@@ -0,0 +1,140 @@
+  
+#include <SFML/Graphics.hpp>
+#include <array>
+#include <vector>
+#include <stdint.h>
+
+static constexpr int IMAGE_WIDTH = 1000;
+static constexpr int IMAGE_HEIGHT = 600;
+
+struct MyColor{
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+};
+
+
+void updateImage(double zoom, double offsetX, double offsetY) ;
+//sf::Color *imageColors = (sf::Color *) calloc(IMAGE_HEIGHT * IMAGE_WIDTH , sizeof(sf::Color));
+static const int MAX = 127; // maximum number of iterations for mandelbrot()
+                         // don't increase MAX or the colouring will look strange
+uint8_t *__restrict imageColors = (uint8_t *) calloc(IMAGE_HEIGHT * IMAGE_WIDTH *4 , sizeof(uint8_t));
+uint8_t *__restrict colors = (uint8_t *) calloc((MAX+1)*3 , sizeof(uint8_t));
+
+    //std::array<sf::Color, MAX+1>__restrict colors;
+    
+int mandelbrot(double startReal, double startImag) ;
+MyColor getColor(int iterations) ;
+void updateImageSlice(double zoom, double offsetX, double offsetY, int minY, int maxY) ;
+
+
+void initColor() {
+    for (int i=0; i <= MAX; ++i) {
+        MyColor color = getColor(i);
+        
+        colors[i*3] = color.red;
+        colors[i*3 + 1] = color.green;
+        colors[i*3 + 2] = color.blue;
+        
+    }
+#pragma acc enter data copyin(colors[:(MAX+1)*3])
+#pragma acc enter data copyin(imageColors[:IMAGE_HEIGHT * IMAGE_WIDTH *4])
+}
+
+int mandelbrot(double startReal, double startImag) {
+    double zReal = startReal;
+    double zImag = startImag;
+
+    for (int counter = 0; counter < MAX; ++counter) {
+        double r2 = zReal * zReal;
+        double i2 = zImag * zImag;
+        if (r2 + i2 > 4.0) {
+            return counter;
+        }
+        zImag = 2.0 * zReal * zImag + startImag;
+        zReal = r2 - i2 + startReal;
+    }
+    return MAX;
+}
+
+MyColor getColor(int iterations){
+    int r, g, b;
+
+    // colour gradient:      Red -> Blue -> Green -> Red -> Black
+    // corresponding values:  0  ->  16  ->  32   -> 64  ->  127 (or -1)
+    if (iterations < 16) {
+        r = 16 * (16 - iterations);
+        g = 0;
+        b = 16 * iterations - 1;
+    } else if (iterations < 32) {
+        r = 0;
+        g = 16 * (iterations - 16);
+        b = 16 * (32 - iterations) - 1;
+    } else if (iterations < 64) {
+        r = 8 * (iterations - 32);
+        g = 8 * (64 - iterations) - 1;
+        b = 0;
+    } else { // range is 64 - 127
+        r = 255 - (iterations - 64) * 4;
+        g = 0;
+        b = 0;
+    }
+    MyColor retVal;
+    retVal.red = r;
+    retVal.green = g;
+    retVal.blue = b;
+    return retVal;
+}
+
+void updateImageSlice(double zoom, double offsetX, double offsetY, int minY, int maxY)
+{
+    double real = 0 * zoom - IMAGE_WIDTH / 2.0 * zoom + offsetX;
+    double imagstart = minY * zoom - IMAGE_HEIGHT / 2.0 * zoom + offsetY;
+
+    #pragma acc parallel loop
+    {
+    for (int x = 0; x < IMAGE_WIDTH; x++, real += zoom) {
+        double imag = imagstart;
+        
+        #pragma acc loop
+        for (int y = minY; y < maxY; y++, imag += zoom) {
+            int value = mandelbrot(real, imag);
+            //imageColors[y*IMAGE_WIDTH + x] = sf::Color(colors[value].r, colors[value].g, colors[value].b);
+            //image.setPixel(x, y, colors[value]);
+            imageColors[(y*IMAGE_WIDTH + x)*4 + 0] = colors[value*3];
+            imageColors[(y*IMAGE_WIDTH + x)*4 + 1] = colors[value*3 + 1];
+            imageColors[(y*IMAGE_WIDTH + x)*4 + 2] = colors[value*3 + 2];
+            imageColors[(y*IMAGE_WIDTH + x)*4 + 3] = 255;
+        }
+        
+    }
+    }
+    
+}
+
+void updateImage(double zoom, double offsetX, double offsetY)
+{
+    const int STEP = IMAGE_HEIGHT;
+    for (int i = 0; i < IMAGE_HEIGHT; i += STEP) {
+        updateImageSlice(zoom, offsetX, offsetY, i, std::min(i+STEP, IMAGE_HEIGHT));
+    }
+}
+
+int main() {
+    double offsetX = -0.7; // and move around
+    double offsetY = 0.0;
+    double zoom = 0.004; // allow the user to zoom in and out...
+    initColor();
+    sf::Image pngImage;
+    updateImage(zoom, offsetX, offsetY);
+    pngImage.create(IMAGE_WIDTH, IMAGE_HEIGHT, imageColors);
+    pngImage.saveToFile("test.png");
+    
+    #pragma acc exit data delete(imageColors[:IMAGE_HEIGHT * IMAGE_WIDTH *4])
+    #pragma acc exit data delete(colors[:(MAX+1)*3])
+    
+    free(imageColors);
+    free(colors);
+
+}
+
